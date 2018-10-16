3bb77d70625a81ebb1aee55295e5cc2bcf568f79
fantasy-is-absent <21972155+fantasy-is-absent@users.noreply.github.com>
2018-10-16 22:28:57 +0300

    Update serializers.py

--- note/serializers.py
+++ note/serializers.py
@@ -3,9 +3,14 @@ from rest_framework import serializers
 from .models import Note
 
 
-class NoteSerializer(serializers.HyperlinkedModelSerializer):
+class NoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Note
         fields = ('note', 'count_unique_words')
-        read_only_fields = ('note', 'count_unique_words',)
+        read_only_fields = ('count_unique_words',)
+
+    def create(self, validated_data):
+        note = Note(note=validated_data['note'])
+        note.save()
+        return note
